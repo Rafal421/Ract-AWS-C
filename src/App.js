ba80741cee2d53ab5f92f554faacd3f1cf0d4d07
Rafal421 <rhaczek421@gmail.com>
2024-04-20 12:10:15 +0200

    First-steps

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { Amplify } from 'aws-amplify';
+import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import config from './amplifyconfiguration.json';
+Amplify.configure(config);
+
+function App() {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
+}
+
+export default withAuthenticator(App);
